d6017d8f9a51726ff96f1abecccd99ec00d009cb
aasiyashafeek <aasiyashafeek15@gmail.com>
2022-07-08 12:02:30 +0530

    viewStudent design completed

--- src/app/view-student/view-student.component.ts
+++ src/app/view-student/view-student.component.ts
@@ -12,4 +12,12 @@ export class ViewStudentComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  data=[
+    {"name":"Anu","age":30,"photo":"https://upload.wikimedia.org/wikipedia/en/2/21/Web_of_Spider-Man_Vol_1_129-1.png"},
+    {"name":"Rahul","age":26,"photo":"https://upload.wikimedia.org/wikipedia/en/3/37/Captain_America_The_First_Avenger_poster.jpg"},
+    {"name":"Sruthi","age":32,"photo":"https://upload.wikimedia.org/wikipedia/en/4/4e/Captain_Marvel_%28film%29_poster.jpg"},
+    {"name":"Sana","age":19,"photo":"https://upload.wikimedia.org/wikipedia/en/2/21/Web_of_Spider-Man_Vol_1_129-1.png"},
+    {"name":"Grey","age":50,"photo":"https://upload.wikimedia.org/wikipedia/en/3/37/Captain_America_The_First_Avenger_poster.jpg"}
+  ]
+
 }
